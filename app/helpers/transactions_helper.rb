6e63fff61a6e5b9ff9dcfbfdfa8d7f2153497e08
Nick Schneble <nschneble@users.noreply.github.com>
2024-08-04 17:02:43 -0400

    add transaction amount formatters

--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -1,3 +1,16 @@
 # Helper methods for transactions
 module TransactionsHelper
+  def transaction_amount_classes(transaction)
+    if transaction.amount.positive?
+      "text-green-700"
+    elsif transaction.amount.negative?
+      "text-red-700"
+    else
+      "text-gray-700"
+    end
+  end
+
+  def format_transaction_amount(transaction)
+    number_to_currency(transaction.amount, unit: "$")
+  end
 end
