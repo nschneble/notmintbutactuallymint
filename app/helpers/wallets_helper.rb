55ea366cc9bf7fdf25d48b52e7ba868039a4b6f6
Nick Schneble <nschneble@users.noreply.github.com>
2024-08-04 17:24:42 -0400

    add top-level navigation

--- app/helpers/wallets_helper.rb
+++ app/helpers/wallets_helper.rb
@@ -18,4 +18,20 @@ module WalletsHelper
       "text-gray-700 active:text-gray-800 hover:bg-gray-100 active:bg-gray-200"
     end
   end
+
+  def nav_item_classes(item, current_page)
+    if item.eql?(current_page)
+      "text-gray-700 underline"
+    else
+      "text-blue-600 hover:text-blue-700 active:text-blue-800 transition-colors duration-200 hover:underline active:underline"
+    end
+  end
+
+  def nav_item_label(item, current_page)
+    if item.eql?(current_page)
+      I18n.t("nav.#{item}")
+    else
+      link_to I18n.t("nav.#{item}"), "#{item}_path"
+    end
+  end
 end
