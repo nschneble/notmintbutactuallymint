2736289a11b33e994515457b6a0876f9fcb44dd7
Nick Schneble <nschneble@users.noreply.github.com>
2024-08-19 22:25:18 -0400

    we don't need these routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,15 +9,6 @@ Rails.application.routes.draw do
     root to: "accounts#index"
   end
 
-  resources :accounts do
-    resources :transactions
-  end
-
-  resources :budgets
-  resources :categories
-  resources :sections
-  resources :transactions
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
